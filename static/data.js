23f5f83e322e5ad235fc3b302cb95b758d9dfa07
hjhcos <3140546263@qq.com>
2021-03-18 22:13:37 +0800

    update

--- static/data.js
+++ static/data.js
@@ -4,22 +4,11 @@ function add0(m) {
 function format(date) {
     let time = new Date(parseInt(date));
     let y = time.getFullYear();
-    let m = time.getMonth();
+    let m = time.getMonth()+1;
     let d = time.getDate();
     return (y + '-' + add0(m) + '-' + add0(d)).toString();
 }
 function realTimeGraph() {// 累计收益图
-    const divStyles = {
-        position: 'absolute',
-        background: 'rgba(255,255,255,0.95)',
-        boxShadow: 'rgb(174, 174, 174) 0px 0px 10px',
-        borderRadius: '4px',
-    };
-    const setStyles = (container, styles) => {
-        for (const key in styles) {
-            container.style[key] = styles[key];
-        }
-    };
     fetch('/static/data.json')
         .then((data) => data.json())
         .then((data) => {
@@ -31,13 +20,13 @@ function realTimeGraph() {// 累计收益图
                 data,
                 xField: 'date',
                 yField: 'earnings',
-                xAxis: {
-                    label: {
-                        formatter: function (data){
-                            return format(data)
+                meta: {
+                    date: {
+                        formatter: (v) => {
+                            return format(v)
                         }
                     }
-                },
+	},
                 seriesField: 'name',
                 interactions: [{type: 'brush'}],
                 tooltip: {
@@ -49,43 +38,6 @@ function realTimeGraph() {// 累计收益图
                 },
             });
             line.render();
-            const createPie = (container, data) => {
-                const piePlot = new Pie(container, {
-                    data,
-                    width: 120,
-                    height: 120,
-                    appendPadding: 10,
-                    autoFit: false,
-                    angleField: 'value',
-                    colorField: 'type',
-                    legend: false,
-                    tooltip: false,
-                    animation: false,
-                    color: line.chart.themeObject.colors10,
-                    label: {
-                        type: 'inner',
-                        offset: '-10%',
-                        content: ({percent}) => `${(percent * 100).toFixed(0)}%`,
-                    },
-                });
-                piePlot.render();
-            };
-            line.update({
-                tooltip: {
-                    customContent: (value, items) => {
-                        const pieData = items.map((item) => ({
-                            type: item.name,
-                            value: Number(item.value),
-                        }));
-                        const container = document.createElement('div');
-                        const pieContainer = document.createElement('div');
-                        setStyles(container, divStyles);
-                        createPie(pieContainer, pieData);
-                        container.appendChild(pieContainer);
-                        return container;
-                    },
-                },
-            });
         });
 }
 function IOPVGraph() {// 单位净值走势图
